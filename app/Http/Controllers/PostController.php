b1ed7ab5033a5e31c05fe45168b956032172e26d
Eñaut AA <enaut.irakasle@gmail.com>
2024-12-12 22:41:39 +0100

    delete eginda

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -67,6 +67,8 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+
+        return ['message' => $post->title.' posta ezabatua'];
     }
 }
