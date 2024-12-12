f3be12b6d554d741ea1144b41dd1a7b56d23566a
Eñaut AA <enaut.irakasle@gmail.com>
2024-12-12 22:30:04 +0100

    store sortuta PostControllerren

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
 use App\Models\Post;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
@@ -15,16 +16,24 @@ class PostController extends Controller
     {
         // $posts = Post::all();
         // return response()->json($posts);
-        
+
         return Post::all();
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StorePostRequest $request)
+    public function store(Request $request)
     {
         //
+       $feilds = $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required'
+        ]);
+
+        $post = Post::create($feilds);
+
+        return ['post' => $post];
     }
 
     /**
